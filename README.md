300879dec349e2a9f75c199bcd8403a754843924
Kyle Ong <83871013+cxyzen@users.noreply.github.com>
2021-10-16 23:47:32 +0800

    Initial commit
    Signed-off-by: Kyle Ong <83871013+cxyzen@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,32 @@
+```
+SET <var> <value>
+GET <var>
+DELETE <var>
+EXISTS <var>
+KEYS *FLUSHALL
+```
+```
+TTL <var>
+EXPIRE <var> <int>
+SETEX <var> <int> <value>
+```
+```
+LPUSH <list> <value>
+LRANGE <list> 0 -1
+RPUSH <list> <value>
+LPOP <list> <value>
+RPOP <list> <value>
+```
+```
+SADD <set> <value>
+SMEMBERS <set>
+SREM <set> <value>
+```
+```
+HSET <hash> <key> <pair>
+HGET <hash> <key>
+HGETALL <hash>
+HDEL <hash> <key> <pair>
+HEXISTS <hash> <key>
+```
+
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const axios = require("axios")
+const cors = require("cors")
+const redis = require("redis")
+
+const DEFAULT_EXPIRATION = 3600
+
+// add url property object for production
+const redisClient = redis.createClient()
+
+const app = express()
+app.use(cors())
+
+app.get("/photos", async (req, res) => {
+    const albumId = req.query.albumId
+    const photos = await getOrSetCache(`photos?albumId=${albumId}`, async () => {
+        const {data} = await axios.get(
+            "https://jsonplaceholder.typicode.com/photos",
+            { params: {albumId}}
+        )
+        return data
+    })
+    res.json(photos)
+})
+
+app.get("/photos/:id", async(req,res) =>{ 
+    const photo = await getOrSetCache(`photos${req.params.id}`, async () => {
+        const {data} = await axios.get(
+            `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+        )
+        return data
+    })
+    res.json(photo)
+})
+
+function getOrSetCache(key, cb){
+    return new Promise((resolve, reject) => {
+        redisClient.get(key, async (error, data) => { 
+            if (error) reject(error)
+            if (data) resolve(JSON.parse(data))
+            const freshData = await cb()
+            redisClient.setex(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
+            resolve(freshData)
+        })
+    })
+}
+
+app.listen(3000, console.log("server start"))
